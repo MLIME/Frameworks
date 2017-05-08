0bdb55d7590b314b0fd0df91fe9dcefd84fd0051
felipessalvatore <felipessalvador@googlemail.com>
2017-05-07 21:51:58 -0300

    Add function for log creation

---
+++ Tensorflow/util.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+
+def newlogname():
+    log_basedir = './graphs'
+    run_label = time.strftime('%d-%m-%Y_%H-%M-%S')  # e.g. 12-11-2016_18-20-45
+    return os.path.join(log_basedir, run_label)
