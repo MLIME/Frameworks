bde27a87046d5fff513c1631e109ab8073a10266
felipessalvatore <felipessalvador@egooglemail.com>
2017-05-08 16:08:54 -0300

    Rename function

--- Tensorflow/util.py
+++ Tensorflow/util.py
@@ -2,7 +2,7 @@ import os
 import time
 
 
-def newlogname():
+def get_log_path():
     log_basedir = './graphs'
     run_label = time.strftime('%d-%m-%Y_%H-%M-%S')  # e.g. 12-11-2016_18-20-45
     return os.path.join(log_basedir, run_label)
